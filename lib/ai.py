174defb2cc5c407a7bced9e8d5690809e35c4762
Andrew Kuskov <qvant86@gmail.com>
2021-02-11 23:26:20 +0300

    Added types

--- lib/ai.py
+++ lib/ai.py
@@ -1,6 +1,7 @@
 class CharAI:
-    def __init__(self, retreat_hp_threshold, retreat_mp_threshold, max_attack_instead_spell, health_potion_gold_percent,
-                 mana_potion_gold_percent, max_hp_percent_to_heal):
+    def __init__(self, retreat_hp_threshold: int, retreat_mp_threshold: int,
+                 max_attack_instead_spell: int, health_potion_gold_percent: int,
+                 mana_potion_gold_percent: int, max_hp_percent_to_heal: int):
         self.retreat_hp_threshold = retreat_hp_threshold
         self.retreat_mp_threshold = retreat_mp_threshold
         self.max_attack_instead_spell = max_attack_instead_spell

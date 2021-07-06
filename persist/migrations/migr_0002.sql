bdea422948d3e84fc97a01630e7fc2ab34920bde
Andrew Kuskov <qvant86@gmail.com>
2021-07-06 23:29:20 +0300

    Update migr_0002.sql

--- persist/migrations/migr_0002.sql
+++ persist/migrations/migr_0002.sql
@@ -1,5 +1,5 @@
 alter table idle_rpg_base.characters alter column telegram_id type bigint;
 alter table idle_rpg_base.feedback_messages alter column telegram_id type bigint;
 alter table idle_rpg_base.feedback_replies alter column telegram_id type bigint;
-update idle_rpg_bot.persist_version set n_version=2, dt_update = current_timestamp where v_name = 'idle RPG' ;
+update idle_rpg_base.persist_version set n_version=2, dt_update = current_timestamp where v_name = 'idle RPG' ;
 commit;
